84893b0ab7e429c4d696dd6dc6a40dd9b53338fc
Researcher <researcher@cse-pc133.oulu.fi>
2016-01-14 11:08:51 +0200

    removed unnecessary imports from MainActivity

--- app/src/main/java/com/example/researcher/heatmap/MainActivity.java
+++ app/src/main/java/com/example/researcher/heatmap/MainActivity.java
@@ -4,16 +4,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Point;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Display;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
